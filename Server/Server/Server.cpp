237d43704cb750de05800b5cdcbdcbae041db28b
cogle <thecogle@gmail.com>
2016-03-24 19:36:15 -0500

    Created Server.

---
+++ Server/Server/Server.cpp
@@ -0,0 +1,14 @@
+// Server.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "common.h"
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "This is the Server" << std::endl;
+    return 0;
+}
+

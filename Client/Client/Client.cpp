d75ead74c084880617ce172e5dcf0824b702af73
cogle <thecogle@gmail.com>
2016-03-24 19:31:09 -0500

    Adding the Client Side.

---
+++ Client/Client/Client.cpp
@@ -0,0 +1,14 @@
+// Client.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "common.h"
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	std::cout << "This is the Client" << std::endl;
+    return 0;
+}
+
